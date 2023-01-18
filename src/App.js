d419df5614f4c9c4599e7a4ad7425a1407166aec
kardesrecep <recepkardes89@gmail.com>
2023-01-18 16:23:19 +0300

    18/01/2023

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-/* import HelloWorld from "./components/01-hello-world/hello-world";
+import { Col, Container, Row } from "react-bootstrap";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/00-home/header/Header";
+import Menu from "./components/00-home/menu/Menu";
+import HelloWorld from "./components/01-hello-world/hello-world";
+import HelloReact from "./components/02-hello-react/hello-react";
 import Jsx1 from "./components/03-jsx/Jsx1";
 import Jsx2 from "./components/03-jsx/Jsx2";
 import Jsx3 from "./components/03-jsx/Jsx3"; 
 import Jsx4 from "./components/03-jsx/Jsx4"; 
 import Jsx5 from "./components/03-jsx/Jsx5"; 
 import Jsx6 from "./components/03-jsx/Jsx6";
-import Style1 from "./components/04-styles/Style1";
+import Jsx7 from "./components/03-jsx/Jsx7"
+/*import Style1 from "./components/04-styles/Style1";
 import Style2 from "./components/04-styles/Style2";
 import Style3 from "./components/04-styles/Style3";
 import Style4 from "./components/04-styles/Style4"
@@ -34,64 +40,158 @@ import UseRef from "./components/21-useref/UseRef";
 import ClassComp from "./components/22-class-type-component/ClassComp"; 
 import UserCards from "./components/23-fetch/UserCards";
 import Countries from "./components/24-axios/Countries";
-import Form1 from "./components/26-forms/Form1";*/
+import Form1 from "./components/26-forms/Form1";
 import Form3 from "./components/26-forms/Form3";
+import Form4 from "./components/26-forms/Form4";
+import Form5 from "./components/26-forms/Form5";
+import Form6 from "./components/26-forms/Form6";*/
+
+
 
 const App = () => {
   return (
-    <div>
-      {/*  <HelloWorld />
-      <Jsx1 />
-      <Jsx2 />
-      <Jsx3 />
-      <Jsx4 />
-      <Jsx5 />
-      <Jsx6 />
-      <Style1 />
-      <Style2 />
-      <Style3 />
-      <Style4 />
-      <Clock1 /> 
-      <Greeting/> 
-      <Products/>  
-      <Clock2 textColor="green" bgColor="yellow" /> 
-      <Image/> 
-  <Gallery/>
-   <ProfileCard
-    avatar="profile.jpg"
-    name="Tanya Kardes"
-    location="Turkey, Istanbul"
-    shot="1243"
-    followers="21232"
-    following="1"
-    /> 
-  <BootstrapStatic/> 
-  <BootstrapDynamic/>  
-  <Icons/>  
-  <Events/> 
-  <Shop/>  
-  <Stateless/> 
-  <State/> 
-  <Counter/> 
-  <Birthday/> 
-  <UseEffect/> 
-  <Clock3/> 
-  <UseEffect2/>
-  <CountryFilter/> 
-  <UseRef/> 
-  <Scroll/>  
-  <ClassComp/>
-<UserCards/> 
-<Countries/>*/}
-<Form3/>
+
+    <BrowserRouter>
+    <Header />
+
+    <Container fluid>
+      <Row>
+        <Col sm={2}>
+          <Menu />
+        </Col>
+        <Col sm={10}>
+          <Routes>
+            <Route path="/hello-world" element={<HelloWorld />} />
+            <Route path="/hello-react" element={<HelloReact />} />
+            <Route path="/jsx1" element={<Jsx1 />} />
+            <Route path="/jsx2" element={<Jsx2 />} />
+            <Route path="/jsx3" element={<Jsx3 />} />
+            <Route path="/jsx4" element={<Jsx4 />} />
+            <Route path="/jsx5" element={<Jsx5 />} />
+            <Route path="/jsx6" element={<Jsx6 />} />
+          </Routes>
+        </Col>
+      </Row>
+    </Container>
 
 
 
 
 
 
-  
-    </div>
+
+
+
+
+
+
+
+
+
+
+  {/*    
+
+   
+  <Jsx6 />
+ <Jsx7/>
+       
+        <Style1/>
+        <Style2/>
+        <Style3/>
+        <Style4/>
+        <Clock1/>
+        <Greetings/>
+        <Products/>
+        <Clock2 textColor="#56ff33" bgColor="#0012C3"/>
+        <Image/>
+        <Gallery/>
+        <ProfileCard avatar="profile.jpg" name="Ali Gel" location="Turkey, Istanbul" shot="3" followers="23432" following="234"/>
+        <BootstrapStatic/>
+        <BootstrapDynamic/>
+        <Icon/>
+        <Events/>
+        <Shop/>
+        <Stateless/>
+        <State/>
+        <Counter/>
+        <Birthday/>
+        <UseEffect1/>
+        <Clock3/>
+        <UseEffect2/>
+        <CountryFilter/>
+        <UseRef/>
+        <Scroll/>
+        <ClassComp/>
+        <UserCards/>
+        <Countries/>
+        <ParentComp/>
+        <Form1/>
+        <Form2/>
+        <Form3/>
+        <Form4/>
+        <Form5/>
+        <Form6/>
+ */}
+    </BrowserRouter>
   );
 };
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ <Jsx6 />
+        <Jsx7/>
+        <Style1/>
+        <Style2/>
+        <Style3/>
+        <Style4/>
+        <Clock1/>
+        <Greetings/>
+        <Products/>
+        <Clock2 textColor="#56ff33" bgColor="#0012C3"/>
+        <Image/>
+        <Gallery/>
+        <ProfileCard avatar="profile.jpg" name="Ali Gel" location="Turkey, Istanbul" shot="3" followers="23432" following="234"/>
+        <BootstrapStatic/>
+        <BootstrapDynamic/>
+        <Icon/>
+        <Events/>
+        <Shop/>
+        <Stateless/>
+        <State/>
+        <Counter/>
+        <Birthday/>
+        <UseEffect1/>
+        <Clock3/>
+        <UseEffect2/>
+        <CountryFilter/>
+        <UseRef/>
+        <Scroll/>
+        <ClassComp/>
+        <UserCards/>
+        <Countries/>
+        <ParentComp/>
+        <Form1/>
+        <Form2/>
+        <Form3/>
+        <Form4/>
+        <Form5/>
+        <Form6/>
+
+
+
+*/
--- src/components/26-forms/Form4.js
+++ src/components/26-forms/Form4.js
@@ -1,13 +1,20 @@
-import React, { useState } from 'react'
-import { Container, Form } from 'react-bootstrap'
+import React, { useState } from "react";
+import { Container, Form } from "react-bootstrap";
 
-const Form3 = () => {
-    const [formData, setFormData] = useState({
-        firstName:"",lastName:"",email:"",phone:""
-    });
+const Form4 = () => {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+  });
 
-    console.log(formData);
+  console.log(formData);
 
+  const handleFormData = (e) => {
+    const { name, value } = e.target;
+    setFormData({...formData, [name]: value});
+  };
 
   return (
     <Container className="mt-5">
@@ -15,42 +22,46 @@ const Form3 = () => {
         <Form.Group className="mb-3">
           <Form.Label>İsim</Form.Label>
           <Form.Control
+            name="firstName"
             type="text"
             placeholder="Adınızı giriniz"
             value={formData.firstName}
-            onChange={(e) => setFormData({...formData, firstName: e.target.value})}
+            onChange={(e) => handleFormData(e)}
           />
         </Form.Group>
         <Form.Group className="mb-3">
           <Form.Label>Soyisim</Form.Label>
           <Form.Control
+            name="lastName"
             type="text"
             placeholder="Soyadınızı giriniz"
             value={formData.lastName}
-            onChange={(e) => setFormData({...formData, lastName: e.target.value})}
+            onChange={(e) => handleFormData(e)}
           />
         </Form.Group>
         <Form.Group className="mb-3">
           <Form.Label>Email</Form.Label>
           <Form.Control
+            name="email"
             type="email"
             placeholder="Epostanızı giriniz"
             value={formData.email}
-            onChange={(e) => setFormData({...formData, email: e.target.value})}
+            onChange={(e) => handleFormData(e)}
           />
         </Form.Group>
         <Form.Group>
           <Form.Label>Telefon</Form.Label>
           <Form.Control
+            name="phone"
             type="text"
             placeholder="Telefonunuzu giriniz"
             value={formData.phone}
-            onChange={(e) => setFormData({...formData, phone: e.target.value})}
+            onChange={(e) => handleFormData(e)}
           />
         </Form.Group>
       </Form>
     </Container>
-  )
-}
+  );
+};
 
-export default Form3
+export default Form4;
